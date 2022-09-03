ce2f2862eff2f17cf23cc213743973bac022a82a
Furkan Emin <femincanx@gmail.com>
2022-09-03 11:28:19 +0300

    feat: add navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import {
   AppBar,
+  Avatar,
   Badge,
   Box,
+  ButtonBase,
   IconButton,
   InputAdornment,
   InputBase,
+  Menu,
+  MenuItem,
   styled,
   Toolbar,
   Typography,
 } from '@mui/material';
-import { Public, Mail, Search } from '@mui/icons-material';
+import { Public, Mail, Search, Notifications } from '@mui/icons-material';
 
 const StyledToolbar = styled(Toolbar)({
   display: 'flex',
@@ -18,18 +23,42 @@ const StyledToolbar = styled(Toolbar)({
 
 const SearchBar = styled(InputBase)(({ theme }) => ({
   backgroundColor: 'white',
-  padding: '0 10px',
+  paddingLeft: '10px',
   borderRadius: theme.shape.borderRadius,
   width: '40%',
   boxShadow: theme.shadows[3],
+  transition: theme.transitions.create(['box-shadow'], {
+    duration: theme.transitions.duration.short,
+  }),
   '&:focus-within': {
     boxShadow: '0 0 0 3px black',
   },
 }));
 
-const Icons = styled(Box)(({ theme }) => ({}));
+const Icons = styled(Box)(({ theme }) => ({
+  display: 'none',
+  alignItems: 'center',
+  gap: '20px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+  },
+}));
+
+const UserBox = styled(ButtonBase)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
+  borderRadius: theme.shape.borderRadius,
+  padding: theme.spacing(1),
+}));
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [anchorElement, setAnchorElement] = useState(null);
+
   return (
     <AppBar position="sticky">
       <StyledToolbar>
@@ -40,7 +69,7 @@ const Navbar = () => {
         <SearchBar
           placeholder="Search"
           endAdornment={
-            <InputAdornment>
+            <InputAdornment position="end">
               <IconButton size="small">
                 <Search />
               </IconButton>
@@ -48,11 +77,61 @@ const Navbar = () => {
           }
         />
         <Icons>
-          <Badge badgeContent={4} color="error">
-            <Mail />
-          </Badge>
+          <IconButton sx={{ color: 'white' }}>
+            <Badge badgeContent={4} color="error">
+              <Mail />
+            </Badge>
+          </IconButton>
+          <IconButton sx={{ color: 'white' }}>
+            <Badge badgeContent={4} color="error">
+              <Notifications />
+            </Badge>
+          </IconButton>
+          <IconButton
+            onClick={(e) => {
+              setIsMenuOpen(true);
+              setAnchorElement(e.currentTarget);
+            }}
+          >
+            <Avatar
+              sx={{ width: 30, height: 30 }}
+              src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg"
+            />
+          </IconButton>
         </Icons>
+        <UserBox
+          onClick={(e) => {
+            setIsMenuOpen(true);
+            setAnchorElement(e.currentTarget);
+          }}
+        >
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg"
+          />
+          <Typography variant="span">John</Typography>
+        </UserBox>
       </StyledToolbar>
+      <Menu
+        open={isMenuOpen}
+        anchorEl={anchorElement}
+        onClose={() => {
+          setIsMenuOpen(false);
+          setAnchorElement(null);
+        }}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My Account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
