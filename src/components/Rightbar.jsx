d54866f0ca35c0a295aec97433811be6c79020f3
Furkan Emin <femincanx@gmail.com>
2022-09-13 22:05:43 +0300

    feat: add content of Rightbar component

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,7 +1,25 @@
-import { Box } from '@mui/material';
-import React from 'react';
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  ImageList,
+  ImageListItem,
+  styled,
+  Typography,
+} from '@mui/material';
+import { usePhotos } from '../context/PhotosContext';
+
+const RightbarHeading = styled(Typography)(({ theme }) => ({
+  fontWeight: 100,
+  marginBottom: theme.spacing(2),
+  '&:not(&:first-of-type)': {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const Rightbar = () => {
+  const { photos } = usePhotos();
+
   return (
     <Box
       sx={{
@@ -13,9 +31,27 @@ const Rightbar = () => {
       <Box
         sx={{
           position: 'fixed',
+          p: 2,
         }}
       >
-        Rightbar
+        <RightbarHeading variant="h5">Online Friends</RightbarHeading>
+        <AvatarGroup max={8} sx={{ justifyContent: 'flex-end' }}>
+          {photos.map((photo, index) => (
+            <Avatar key={index} src={photo.user.profile_image.large} />
+          ))}
+        </AvatarGroup>
+        <RightbarHeading variant="h5">Latest Photos</RightbarHeading>
+        <ImageList variant="masonry" cols={3} gap={5}>
+          {photos.slice(5, 12).map((photo, index) => (
+            <ImageListItem key={index}>
+              <img
+                src={photo.urls.small}
+                alt={photo.alt_description}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
       </Box>
     </Box>
   );
